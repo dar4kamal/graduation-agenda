05de2deece9a7daa463e9b77ea857896e4db682a
Mostafa Kamal <34961537+dar4kamal@users.noreply.github.com>
2024-02-18 14:13:49 +0200

    updated image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
       <div className="h-full md:h-auto flex items-center justify-center bg-[#144476]">
         <img
           className="w-auto max-h-full"
-          src="assets/agenda3.jpg"
+          src="assets/ICCIIS_CFP.jpg"
           alt="Grad Agenda"
         />
       </div>
